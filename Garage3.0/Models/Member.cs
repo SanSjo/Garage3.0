d9340ded1714f739d942fb8c7122357a1bf082cf
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-03-24 16:59:56 +0100

    Added autogenerated pk to member class

--- Garage3.0/Models/Member.cs
+++ Garage3.0/Models/Member.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Garage3._0.Models
 {
     public class Member
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MemberPK { get; set; }
+
         public int IDNumber { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
         public DateTime Joined { get; set; }
+
         public DateTime? ExtendedMemberShipEndDate { get; set; }
     }
 }
