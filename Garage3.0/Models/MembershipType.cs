a64c40661290fb112bcfffc52c8ae1a56f7dd418
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-03-24 16:36:52 +0100

    Added Class Membershiptype

---
+++ Garage3.0/Models/MembershipType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3._0.Models
+{
+    public class MembershipType
+    {
+        public string Type { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
