52ef50a57fd87711ecfba751a445daf9616f0617
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-03-25 21:36:25 +0100

    added administration controller and view

---
+++ Garage3.0/Controllers/AdministrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garage3.Data;
+using Garage3.Models;
+
+namespace Garage3.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly Garage3Context db;
+
+        public AdministrationController(Garage3Context context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
