1aa48bb3b006c115d0d427a8034e2b418b94ab9d
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-03-25 21:37:12 +0100

    Added statistics controller and view

---
+++ Garage3.0/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Garage3.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Garage3.Controllers
+{
+    public class StatisticsController : Controller
+    {        
+            private readonly Garage3Context db;
+
+            public StatisticsController(Garage3Context context)
+            {
+                db = context;
+            }
+            public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
