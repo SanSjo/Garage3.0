f532efda17ae1b0def340e49426bfda33fe56ae2
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-03-27 10:42:44 +0100

    Added requirement that the name is at least 2 chars long

--- Garage3.0/Models/Member.cs
+++ Garage3.0/Models/Member.cs
@@ -17,8 +17,10 @@ namespace Garage3.Models
         public string PersonalIdentityNumber { get; set; }
 
         // TODO: Add check that first name isnt the same as last name
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters long.")]
         public string FirstName { get; set; }
 
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters long.")]
         public string LastName { get; set; }
 
         public MembershipType MembershipType { get; set; }
