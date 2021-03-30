787e72e4a64e383d150d7b029fe76f0352420ccd
Susanna Sjöberg <susanna.sjoberg@gmail.com>
2021-03-30 11:27:15 +0200

    Update Edit and add modal to Edit

--- Garage3.0/wwwroot/js/scripts.js
+++ Garage3.0/wwwroot/js/scripts.js
@@ -1,1 +1,9 @@
-﻿
+﻿//If parking form is valid the confirmation modal opens
+
+$("#submit-button").click(function (event) {
+    event.preventDefault()
+
+    if ($('#form').valid()) {
+        $('.modal').modal('show')
+    }
+});
